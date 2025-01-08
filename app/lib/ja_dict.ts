3ce42e9f222a91b5033ef0c8a3be265d4224c397
Nitin Nazeer <nitin.nazeer@gmail.com>
2025-01-07 21:58:56 -0500

    explicit path to /public/dict using process.cwd()

--- app/lib/ja_dict.ts
+++ app/lib/ja_dict.ts
@@ -54,7 +54,7 @@ function compareEntries(a: Entry, b: Entry, word: string): number {
 
 const tokenizer = new Promise((resolve, reject) => {
     console.log(process.cwd())
-    kuromoji.builder({ dicPath: `${process.cwd()}/node_modules/kuromoji/dict/` }).build((err: any, tokenizer: any) => {  // eslint-disable-line
+    kuromoji.builder({ dicPath: `${process.cwd()}/public/dict/` }).build((err: any, tokenizer: any) => {  // eslint-disable-line
       if (err) reject(err);
       else resolve(tokenizer);
     });
